3240dacd5c9a247c943be83382147474fb6f2582
BrianPhillipps <110609951+BrianPhillipps@users.noreply.github.com>
2023-01-31 01:11:03 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,116 +1,117 @@
 // firebase connection
 var firebaseConfig = {
-  apiKey: "AIzaSyAVfWpeD9wGo0_WjL3U3Vl8rntODLn8Thc",
-  authDomain: "doordash-dining-hall.firebaseapp.com",
-  databaseURL: "https://doordash-dining-hall-default-rtdb.firebaseio.com",
-  projectId: "doordash-dining-hall",
-  storageBucket: "doordash-dining-hall.appspot.com",
-  messagingSenderId: "1017765572736",
-  appId: "1:1017765572736:web:e6237d4801c629f0c5f665",
+	apiKey: "AIzaSyAVfWpeD9wGo0_WjL3U3Vl8rntODLn8Thc",
+	authDomain: "doordash-dining-hall.firebaseapp.com",
+	databaseURL: "https://doordash-dining-hall-default-rtdb.firebaseio.com",
+	projectId: "doordash-dining-hall",
+	storageBucket: "doordash-dining-hall.appspot.com",
+	messagingSenderId: "1017765572736",
+	appId: "1:1017765572736:web:e6237d4801c629f0c5f665",
 };
 
 firebase.initializeApp(firebaseConfig);
 var database = firebase.database();
 
-
-
 // form submission
 var currentDate = new Date();
 var deliversAvailable = 20;
 async function submitForm() {
-  var date = currentDate.toString();
-  var selectedTime = new Date();
-  var hour = selectedTime.getHours();
-  var name = document.getElementById("name").value;
-  var email = document.getElementById("email").value;
-  var id = document.getElementById("studentID").value;
-  var dorm = document.getElementById("dorm").value;
-  var meal = document.getElementById("meal").value;
-  var room = document.getElementById("room").value;
-  var time = document.getElementById("time").value;
-  var phone = document.getElementById("phone").value;
-  var ref = firebase.database().ref("times/" + time);
-  var timeRef = firebase.database().ref("times");
-  var emailRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@(kent.edu)$/;
-  var idRegex = /^\d+$/;
-  selectedTime.setHours(time.split(':')[0], time.split(':')[1], 0);
+	var date = currentDate.toString();
+	var selectedTime = new Date();
+	var name = document.getElementById("name").value;
+	var email = document.getElementById("email").value;
+	var id = document.getElementById("studentID").value;
+	var dorm = document.getElementById("dorm").value;
+	var meal = document.getElementById("meal").value;
+	var room = document.getElementById("room").value;
+	var time = document.getElementById("time").value;
+	var phone = document.getElementById("phone").value;
+	var ref = firebase.database().ref("times/" + time);
+	var timeRef = firebase.database().ref("times");
+	var emailRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@(kent.edu)$/;
+	var idRegex = /^\d+$/;
+	selectedTime.setHours(time.split(":")[0], time.split(":")[1], 0);
 
-
-  if (name == "" || email == "" || id == "" || dorm == "" || meal == "" || dorm == "" || room == "" || phone == "") {
-    alert("Please fill in all fields");
-  }
-    else if (!emailRegex.test(email)) {
-    alert("Please enter a valid Kent email address");
-  } else if (!idRegex.test(id)) {
-    alert("Please enter a valid student ID");
-  } else if (id < 100000000 || id > 999999999) {
-    alert("Please enter a valid student ID");
-  } else if (selectedTime.getHours() > 20) {
-    alert("Delivery is closed. Come back tomorrow and order between 6-8 P.M.");
-  } else if (selectedTime < currentDate) {
-    alert("Selected time has already passed, please select a valid time");
-  } 
-   else {
-    var studentRef = firebase.database().ref("students");
-    const studentSnapshot = await studentRef.child(id).once("value");
-    if(studentSnapshot.exists()) {
-      alert("You have an existing order already.");
-      return;
-    }
-    const timeSnapshot = await timeRef.child(time).once("value");
-    var count = timeSnapshot.numChildren();
-    if (count >= deliversAvailable) {
-      alert(
-        "Sorry, this dinner is full. Please select another time or wait until tomorrow."
-      );
-    } else {
-      ref.child(id).set({
-        id: id,
-        date: date,
-        name: name,
-        phone: phone,
-        time: time,
-        room: room,
-        meal: meal,
-        email: email,
-        dorm: dorm
-      });
-      studentRef.child(id).set({
-        id: id,
-        name: name,
-        email: email
-      });
-      window.location = "deliverypage.html";
-    }
-  }
+	if (
+		name == "" ||
+		email == "" ||
+		id == "" ||
+		dorm == "" ||
+		meal == "" ||
+		dorm == "" ||
+		room == "" ||
+		phone == ""
+	) {
+		alert("Please fill in all fields");
+	} else if (!emailRegex.test(email)) {
+		alert("Please enter a valid Kent email address");
+	} else if (!idRegex.test(id)) {
+		alert("Please enter a valid student ID");
+	} else if (id < 100000000 || id > 999999999) {
+		alert("Please enter a valid student ID");
+	} else if (selectedTime.getHours() > 20) {
+		alert("Delivery is closed. Come back tomorrow and order between 6-8 P.M.");
+	} else if (selectedTime < currentDate) {
+		alert("Selected time has already passed, please select a valid time");
+	} else {
+		var studentRef = firebase.database().ref("students");
+		const studentSnapshot = await studentRef.child(id).once("value");
+		if (studentSnapshot.exists()) {
+			alert("You have an existing order already.");
+			return;
+		}
+		const timeSnapshot = await timeRef.child(time).once("value");
+		var count = timeSnapshot.numChildren();
+		if (count >= deliversAvailable) {
+			alert(
+				"Sorry, this dinner is full. Please select another time or wait until tomorrow."
+			);
+		} else {
+			studentRef.child(id).set({
+				id: id,
+				name: name,
+				email: email,
+			});
+			ref.child(id).set({
+				id: id,
+				date: date,
+				name: name,
+				phone: phone,
+				time: time,
+				room: room,
+				meal: meal,
+				email: email,
+				dorm: dorm,
+			});
+			window.localStorage.setItem("time", time);
+			window.location = "deliverypage.html";
+		}
+	}
 }
-
-
-
 
 // clear database
 function clearDatabase() {
-  database.ref("times").remove();
-  alert("database cleared");
+	database.ref("times").remove();
+	alert("database cleared");
 }
 
 // Page popup
- let popup = document.getElementById("popup");
+let popup = document.getElementById("popup");
 
- function openpopup(){
-  popup.classList.add("open-popup");
- }
+function openpopup() {
+	popup.classList.add("open-popup");
+}
 
- function closepopup(){
-  popup.classList.remove("open-popup");
- }
- 
+function closepopup() {
+	popup.classList.remove("open-popup");
+}
+
 // loading page
-$(document).ready(function(){
-  $(".content").hide();
-  setTimeout(function() {
-    $(".loader-wrapper").fadeOut("slow", function(){
-        $(".content").fadeIn("slow");
-    });
-  }, 2500);
+$(document).ready(function () {
+	$(".content").hide();
+	setTimeout(function () {
+		$(".loader-wrapper").fadeOut("slow", function () {
+			$(".content").fadeIn("slow");
+		});
+	}, 2500);
 });
